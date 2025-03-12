5f5c5fbafdd09e9fe46a2e4d1921c7cfb5483feb
Your Judah <jd002.trbl7@gmail.com>
2025-03-12 09:38:15 -0300

    importação de 'styles.css'

--- Front/Cart/src/App.jsx
+++ Front/Cart/src/App.jsx
@@ -1,4 +1,5 @@
-import './App.css'
+// import './App.css'
+import './assets/styles.css' // O professor tinha recomendado o uso desse estilo
 import {BrowserRouter, Router, Route} from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
