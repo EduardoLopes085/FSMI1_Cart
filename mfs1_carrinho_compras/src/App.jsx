5660e0a557f359cedf03e238618e4dc280d419f7
Your Judah <jd002.trbl7@gmail.com>
2025-03-12 09:02:23 -0300

    instalação do axios

--- mfs1_carrinho_compras/src/App.jsx
+++ mfs1_carrinho_compras/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {BrowserRouter, Route, Router} from 'react-router-dom'
+import axios, {isCancel, AxiosError} from 'axios';
 function App() {
   const [count, setCount] = useState(0)
 
