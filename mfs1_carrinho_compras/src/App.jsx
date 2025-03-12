7cfe8e1bb80ef13c4ea8274dbbc8c1532a3b95aa
Your Judah <jd002.trbl7@gmail.com>
2025-03-12 09:05:19 -0300

    remoçao das importações das imagens do vite

--- mfs1_carrinho_compras/src/App.jsx
+++ mfs1_carrinho_compras/src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {BrowserRouter, Route, Router} from 'react-router-dom'
 import axios, {isCancel, AxiosError} from 'axios';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
