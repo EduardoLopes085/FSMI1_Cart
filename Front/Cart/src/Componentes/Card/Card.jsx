33c867efac95e6aa64992aae63ed1d4eb8667227
Eduardo Lopes <eduardolcb18@gmail.com>
2025-03-14 22:45:01 -0300

    correcoes css

--- Front/Cart/src/Componentes/Card/Card.jsx
+++ Front/Cart/src/Componentes/Card/Card.jsx
@@ -14,7 +14,7 @@ const Card = ({ id, image, title, price}) => {
     <div className="single-product">
       <img src={image} alt={title} className="product-image" />
       <h2 className="product-title">{title}</h2>
-      <p>{price ? price.toFixed(2) : "Preço não disponível"}</p>
+      <p className='product-price' >{price ? price.toFixed(2) : "Preço não disponível"}</p>
       {/* <Link to={`/product/${id}`} className="details-button" onClick={onDetailsClick}>Exibir Mais Detalhes</Link> */}
       <button className="details-button" onClick={handleDetailsClick}>
         Exibir Mais Detalhes
